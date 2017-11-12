be81f55ba9b933d0a9d1a39860f4e714f4373175
PorryChen <porrychen@gmail.com>
2017-11-12 02:48:49 -0600

    lab5b change a function to a loop

--- 4.lab5/Lab5b.py
+++ 4.lab5/Lab5b.py
@@ -7,42 +7,44 @@ DELUXE_PRICE  = 15
 
 # The main function.
 def main():
-    # call ask_tickets function to get the number of each category
-    matinee_tickets = ask_tickets('Matinee')
-    print()
-    regular_tickets = ask_tickets('Regular')
-    print()
-    deluxe_tickets = ask_tickets('Deluxe')
+    category_names = ['Matinee', 'Regular', 'Deluxe']
+    category_tickets = [0, 0, 0]
+    count = 0
+    again = False
+
+    # loop for getting the number of each category
+    while count < len(category_names):
+        tickets = 0
+        # asks how many tickets are sold for the category
+        try:
+            if not again:
+                print('How many ' + category_names[count] + '\'s tickets are sold?')
+
+            tickets = int(input('Please ' + ('re-' if again else '') + 'enter a number: '))
+
+            # validate the tickets
+            if tickets < 0:
+                print('\n⛔ ERROR: Number of ' + category_names[count] + '\'s tickets cannot be negative.', end = '\n\n')
+                again = True
+            else:
+                category_tickets[count] = tickets
+
+                again = False
+                count += 1
+                print()
+        except ValueError:
+            print('\n⛔ ERROR: Number of ' + category_names[count] + '\'s tickets must be valid numbers.', end = '\n\n')
+
+            again = True
 
     # call calculate_revenue
-    calculate_revenue(matinee_tickets, regular_tickets, deluxe_tickets)
+    calculate_revenue(category_tickets[0], category_tickets[1], category_tickets[2])
 
     # display ending words
     print()
     print('⬐-----------------------------⬎')
     print('    Bye! See you next time!   ')
     print('⬑-----------------------------⬏')
-
-# The ask_tickets function for asking users to enter a number.
-def ask_tickets(categoryName, again = False):
-    tickets = 0
-    # asks how many tickets are sold for the category 
-    try:
-        if not again:
-            print('How many ' + categoryName + '\'s tickets are sold?')
-            
-        tickets = int(input('Please ' + ('re-' if again else '') + 'enter a number: '))
-
-        # validate the tickets
-        if tickets < 0:
-            print('\n⛔ ERROR: Number of ' + categoryName + '\'s tickets cannot be negative.', end = '\n\n')
-            tickets = ask_tickets(categoryName, True)
-            
-    except ValueError:
-        print('\n⛔ ERROR: Number of ' + categoryName + '\'s tickets must be valid numbers.', end = '\n\n')
-
-        tickets = ask_tickets(categoryName, True)
-    return tickets
 
 # The calculate_revenue function for calculating total revenue.
 def calculate_revenue(matineeTickets, regularTickets, deluxeTickets):
