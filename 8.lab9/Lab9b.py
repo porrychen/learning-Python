2f493ec8b79d5bcfa641040dbe7392c5bf75fe62
porry <porrychen@gmail.com>
2017-12-13 15:21:43 -0600

    update lab9b

--- 8.lab9/Lab9b.py
+++ 8.lab9/Lab9b.py
@@ -7,18 +7,22 @@ MONTH_NAMES = ["January", "Febuary", "March", "April", "May", "June",
 
 # The main function.
 def main():
+    # initialize a flag 
     done = False
 
+    # if not done, then continue
     while not done:
         date = ''
         while date == '':
+            # input a date in numeric format
             date = input('Please enter a date(mm/dd/yy): ')
-            show_date = format_date(date)
-            if show_date != '':
+
+            # check the date is valid
+            date = check_date(date)
+            
+            if date != '':
                 # output the date in long date format
-                print(show_date)
-            else:
-                date = ''
+                show_date(date)
 
         # ask the user whether try again
         done = input('\nDo you want to enter a new date? (Y/n)').lower() == 'n'
@@ -28,7 +32,8 @@ def main():
     print(' ⭐  Bye! See you next time!       ⭐ ')
     print('⬑-----------------------------------⬏')
 
-def format_date(date):
+# The check_date function.
+def check_date(date):
     correct = False
 
     date_list = date.split('/')
@@ -37,15 +42,6 @@ def format_date(date):
         if date_list[0].isdigit() and int(date_list[0]) in range(1, 13):
             if date_list[1].isdigit() and int(date_list[1]) in range(1, 32):
                 if date_list[2].isdigit() and len(date_list[2]) == 2:
-                    date_list[2] = int(date_list[2]) + 2000
-                    date_list.append('%s %d, %s' % (MONTH_NAMES[int(date_list[0]) - 1], int(date_list[1]), date_list[2]))
-
-                    # use calendar module to further check
-                    # first day of the month and number of days in month
-                    month_range = calendar.monthrange(date_list[2], int(date_list[0]))
-                    if int(date_list[1]) > month_range[1]:
-                        date_list[-1] += '\nActually, the max day is ' + str(month_range[1])
-
                     correct = True
                 else:
                     print('⭕ ERROR: The year(yy) must be two digits.')
@@ -55,8 +51,22 @@ def format_date(date):
             print('⭕ ERROR: The month(mm) must be an integer in the range 1 through 12.')
     elif date != '':
         print('⭕ ERROR: Must be a correct format of a date!')
+    
+    return date if correct else ''
 
-    return date_list[-1] if correct else ''
+# The show_date function.
+def show_date(date):
+    date_list = date.split('/')
+
+    date_list[2] = int(date_list[2]) + 2000
+    # output the date in long date format
+    print('\n%s %d, %s' % (MONTH_NAMES[int(date_list[0]) - 1], int(date_list[1]), date_list[2]))
+
+    # use calendar module to further check
+    # first day of the month and number of days in month
+    month_range = calendar.monthrange(date_list[2], int(date_list[0]))
+    if int(date_list[1]) > month_range[1]:
+        print('Actually, the max day is ' + str(month_range[1]))
 
 # Call the main function.
 main()
