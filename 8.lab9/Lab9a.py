b7c20b9fd327c9054c39949c9e4c1104c1e81eb3
porry <porrychen@gmail.com>
2017-12-13 15:43:09 -0600

    update lab9a

--- 8.lab9/Lab9a.py
+++ 8.lab9/Lab9a.py
@@ -6,34 +6,27 @@ def main():
         email_address = ''
 
         while email_address == '':
-            # input the email address in a validation loop
+            # 1. input the email address in a validation loop
             email_address = input('Please enter a student\'s email address(firstname.lastname@g.austincc.edu): ')
 
-            # validate the address
+            # 2. validate the address
             if not check_email(email_address):
                 email_address = ''
             else:
-                # extract the student’s first name and last name from the email address
-                name_list = email_address.split('@')[0].split('.')
-                if len(name_list[0]) > 0:
-                    # capitalize the first letter of firstname
-                    name_list[0] = name_list[0][0].upper() + (name_list[0][1:] if len(name_list[0]) > 1 else '')
-                if len(name_list) > 1 and len(name_list[-1]) > 0:
-                    # capitalize the first letter of lastname
-                    name_list[-1] = name_list[-1][0].upper() + (name_list[-1][1:] if len(name_list[-1]) > 1 else '')
-                else:
-                    name_list.append(' ')
-                # display the student’s name
-                print('The student’s name is', name_list[0], name_list[-1])
+                # 3. extract the student’s first name and last name from the email address
+                name_list = extract_email(email_address)
+                # 4. display the student’s name
+                show_name(name_list)
 
         # ask the user whether try again
-        done = input('Do you want to enter a new student\'s email address? (Y/n)').lower() == 'n'
+        done = input('\nDo you want to enter a new student\'s email address? (Y/n)').lower() == 'n'
 
     print('⬐-----------------------------------⬎')
     print(' ⭐  Thank you for using this app! ⭐ ')
     print(' ⭐  Bye! See you next time!       ⭐ ')
     print('⬑-----------------------------------⬏')
 
+# The check_email function.
 def check_email(email):
     result = False
 
@@ -48,6 +41,8 @@ def check_email(email):
     elif email != '':
         if period_index < 0:
             print('⭕ ERROR: Use a period sign to separate the first and last names!')
+        elif period_index == 0:
+            print('⭕ ERROR: Must have a first or last name!')
         elif sign_index < 0:
             print('⭕ ERROR: Must be a @ sign in an email address!')
         else:
@@ -57,5 +52,28 @@ def check_email(email):
 
     return result
 
+# The extract_email function.
+def extract_email(email):
+    # extract the student’s first name and last name from the email address
+    name_list = email.split('@')[0].split('.')
+    
+    if len(name_list[0]) > 0:
+        # capitalize the first letter of firstname
+        name_list[0] = name_list[0][0].upper() + (name_list[0][1:] if len(name_list[0]) > 1 else '')
+    if len(name_list) > 1 and len(name_list[-1]) > 0:
+        # capitalize the first letter of lastname
+        name_list[-1] = name_list[-1][0].upper() + (name_list[-1][1:] if len(name_list[-1]) > 1 else '')
+    else:
+        name_list.append('')
+
+    return name_list
+
+# The show_name function.
+def show_name(name_list):
+    name = '%s %s' % (name_list[0], name_list[-1])
+
+    # display the student’s name
+    print('The student’s name:', name.strip())
+    
 # Call the main function.
 main()
